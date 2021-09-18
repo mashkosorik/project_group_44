9106f8104b4709cb8cf412e7976f2c45309119e1
AndriiShliakhta <andrew_shliahta@ukr.net>
2021-09-18 10:20:11 +0300

    Update firstLoading.js

--- src/js/firstLoading.js
+++ src/js/firstLoading.js
@@ -6,7 +6,7 @@ import { refs } from "./refs";
 export function firstLoadding() {
     axios
       .get(
-        `https://app.ticketmaster.com/discovery/v2/events.json?}&page=${refs.page}&apikey=MrDjiKw1cBGuG57562zYpO5puccpSyZ6`,
+        `https://app.ticketmaster.com/discovery/v2/events.json?&page=${refs.page}&apikey=MrDjiKw1cBGuG57562zYpO5puccpSyZ6`,
       )
       .then((resp) => {
           createMarkup(resp);
